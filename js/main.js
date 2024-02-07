1defd57888d7f6d16df258bf661adc80d6d6332e
Lesha <lexaxiip2gmail.com>
2024-02-07 15:51:38 +0200

    Added form and footer

--- js/main.js
+++ js/main.js
@@ -1,1 +1,42 @@
 AOS.init();
+
+
+$('#phone').on('input', function () {
+  var val = this.value;
+  if (val.length < 4)
+    this.value = '+380';
+});
+
+
+
+
+const mailPath = './mail.php'
+
+document.querySelectorAll('.uniForm').forEach((e) => {
+
+  e.addEventListener('submit', function (e) {
+
+    let th = this,
+      params = new FormData(this),
+      request = new XMLHttpRequest()
+
+    request.open('POST', mailPath, true)
+    request.send(params)
+
+    request.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        setTimeout(function () { th.reset() }, 1000)
+        const modal = document.querySelector('.modal_thank');
+        const close = document.querySelector('.modal-close');
+        modal.style.display = 'block';
+        close.addEventListener('click', () => {
+          modal.style.display = 'none';
+        })
+      }
+    }
+
+    e.preventDefault()
+
+  })
+
+})
